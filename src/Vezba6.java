b466914287585b8efe08a0eba1e6378401b00212
Стефан Најдовски <83938539+stefan-najdovski@users.noreply.github.com>
2022-10-27 18:19:14 +0200

    refactor Vezba6.java

--- src/Vezba6.java
+++ src/Vezba6.java
@@ -14,9 +14,9 @@ public class Vezba6 {
             // so literals moze i vo var
 
             System.out.println("Сметка:");
-            System.out.println("Едно чоколадо -"+proizvodi.article1.toLowerCase() +" "+ proizvodi.prize1 +" "+ proizvodi.currency+"и");
+            System.out.println("Едно "+proizvodi.article1.toLowerCase() +" "+ proizvodi.prize1 +" "+ proizvodi.currency+"и");
             System.out.println(price1Double + " x "+ price2Double + " = " + price1Double*quantity);
-            System.out.println("Еден сок " + proizvodi.article2.toLowerCase() +" "+proizvodi.prize2 +" "+ proizvodi.currency+"и");
+            System.out.println("Еден " + proizvodi.article2.toLowerCase() +" "+proizvodi.prize2 +" "+ proizvodi.currency+"и");
             System.out.println(price1Double + " x "+ price2Double + " = " + price2Double*quantity);
             System.out.println("Вкупна Цена:" + sum + " "+ proizvodi.currency+"и");
 
