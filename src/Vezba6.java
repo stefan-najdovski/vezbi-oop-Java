914e1015dc2cc1764f3a62d1215fdf26c6bd4c70
Стефан Најдовски <83938539+stefan-najdovski@users.noreply.github.com>
2022-10-27 18:17:37 +0200

    refactor Vezba6.java

--- src/Vezba6.java
+++ src/Vezba6.java
@@ -1,5 +1,3 @@
-import java.sql.Struct;
-
 public class Vezba6 {
 
     public static void main(String[] args){
@@ -11,14 +9,16 @@ public class Vezba6 {
         Double price2Double = Double.parseDouble(proizvodi.prize2);
         Double quantity = Double.parseDouble(proizvodi.quantity);
         Double sum = price1Double*quantity + price2Double*quantity;
-    // so literals moze i vo var
 
-    System.out.println("Сметка:");
-    System.out.println("Едно чоколадо -"+proizvodi.article1.toLowerCase() +" "+ proizvodi.prize1 +" "+ proizvodi.currency+"и");
-    System.out.println(price1Double + " x "+ price2Double + " = " + price1Double*quantity);
-    System.out.println("Еден сок " + proizvodi.article2.toLowerCase() +" "+proizvodi.prize2 +" "+ proizvodi.currency+"и");
-    System.out.println(price1Double + " x "+ price2Double + " = " + price2Double*quantity);
-    System.out.println("Вкупна Цена:" + sum + " "+ proizvodi.currency+"и");
 
-    }
+            // so literals moze i vo var
+
+            System.out.println("Сметка:");
+            System.out.println("Едно чоколадо -"+proizvodi.article1.toLowerCase() +" "+ proizvodi.prize1 +" "+ proizvodi.currency+"и");
+            System.out.println(price1Double + " x "+ price2Double + " = " + price1Double*quantity);
+            System.out.println("Еден сок " + proizvodi.article2.toLowerCase() +" "+proizvodi.prize2 +" "+ proizvodi.currency+"и");
+            System.out.println(price1Double + " x "+ price2Double + " = " + price2Double*quantity);
+            System.out.println("Вкупна Цена:" + sum + " "+ proizvodi.currency+"и");
+
+        }
     }
