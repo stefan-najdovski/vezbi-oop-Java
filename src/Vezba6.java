61721dcb224a87a389161e60bb389aa2253f33d4
Стефан Најдовски <83938539+stefan-najdovski@users.noreply.github.com>
2022-10-27 18:16:58 +0200

    fix Vkupna cena

--- src/Vezba6.java
+++ src/Vezba6.java
@@ -10,7 +10,7 @@ public class Vezba6 {
         Double price1Double = Double.parseDouble(proizvodi.prize1);
         Double price2Double = Double.parseDouble(proizvodi.prize2);
         Double quantity = Double.parseDouble(proizvodi.quantity);
-
+        Double sum = price1Double*quantity + price2Double*quantity;
     // so literals moze i vo var
 
     System.out.println("Сметка:");
@@ -18,6 +18,7 @@ public class Vezba6 {
     System.out.println(price1Double + " x "+ price2Double + " = " + price1Double*quantity);
     System.out.println("Еден сок " + proizvodi.article2.toLowerCase() +" "+proizvodi.prize2 +" "+ proizvodi.currency+"и");
     System.out.println(price1Double + " x "+ price2Double + " = " + price2Double*quantity);
-    System.out.println("Вкупна Цена:" + price1Double*quantity + price2Double*quantity);
+    System.out.println("Вкупна Цена:" + sum + " "+ proizvodi.currency+"и");
+
     }
     }
